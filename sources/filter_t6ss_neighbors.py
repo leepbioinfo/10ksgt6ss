d1b40e69f7cde6c46b2df4d9efc389a468e93f6a
Robson Francisco de Souza <rfsouza@usp.br>
2025-07-12 21:25:47 -0300

    Fixing STox_19

---
+++ sources/filter_t6ss_neighbors.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+df = pd.read_pickle('../../data/genome.pkl')
+df.loc[df.rocha == 'T6SSi_tssH', 't6ss'] = False
+df.loc[df.rocha == 'T6SSiii_tssH+T6SSi_tssH', 't6ss'] = False
+df.loc[df.rocha == 'T6SSi_tssH+T6SSiii_tssH', 't6ss'] = False
+t6 = df[df.t6ss ==True].t6ss
+df2 = df.neighbors(t6, after=10, before=10, min_block_distance=4)
+df2.to_pickle('vizinhos_t6_novo.pkl')
